b9720ea480592f7efabc9de9bcd1abdad8d9c534
Niklas Mähler <niklas.mahler@gmail.com>
2019-10-08 09:23:22 +0200

    Add latest assembly results

--- js/customise.js
+++ js/customise.js
@@ -1,11 +1,15 @@
 Reveal.addEventListener("fragmentshown", function(event) {
     if (event.fragment.id === "spruce-shrink-fragment") {
         document.getElementById("spruce-shrink-img").classList.add("shrink");
+    } else if (event.fragment.id === "spruce-shrink-fragment2") {
+        document.getElementById("spruce-shrink-img2").classList.add("shrink-more");
     }
 });
 
 Reveal.addEventListener("fragmenthidden", function(event) {
     if (event.fragment.id === "spruce-shrink-fragment") {
         document.getElementById("spruce-shrink-img").classList.remove("shrink");
+    } else if (event.fragment.id === "spruce-shrink-fragment2") {
+        document.getElementById("spruce-shrink-img2").classList.remove("shrink-more");
     }
 });
